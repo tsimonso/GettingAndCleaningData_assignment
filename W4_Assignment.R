b0b7cffa6763977ce71f721cd9874aad653ac851
Thomas Simonson <thomas.simonson@gmail.com>
2020-05-11 23:20:34 +0200

    Create W4_Assignment.R

---
+++ W4_Assignment.R
@@ -0,0 +1,13 @@
+
+## The submitted data set is TIDY
+## The Github repo contains the required SCRIPTS
+## GitHub contains a CODE BOOK that modifies and updates the available code book with the data to indicate all the variables and summaries calculated, along with units, and any other relevant information.
+## The README that explains the analysis files is clear and understandable.
+
+##submit: 
+##1) a tidy data set as described below, 
+##2) a link to a Github repository with your script for performing the analysis, and 
+##3) a code book that describes the variables, the data, and any transformations or work that you performed to clean up the data called CodeBook.md. 
+##You should also include a README.md in the repo with your scripts. This repo explains how all of the scripts work and how they are connected.
+
+url<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
